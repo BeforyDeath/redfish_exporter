5fc2029d97bdc70d92b3e3d4a63eb4dce006a98b
byd <grishin.aleksey7@wb.ru>
2023-07-11 12:02:32 +0200

    fix duplicate device name

--- collector/system_collector.go
+++ collector/system_collector.go
@@ -336,6 +336,9 @@ func (s *SystemCollector) Collect(ch chan<- prometheus.Metric) {
 					devices := simpleStorage.Devices
 					wg8 := &sync.WaitGroup{}
 					wg8.Add(len(devices))
+
+					devices = duplicateDevices(devices) // todo if device.Name duplicate, set "device.Name (index++)"
+
 					for _, device := range devices {
 						go parseDevice(ch, systemHostName, device, wg8)
 					}
@@ -542,4 +545,30 @@ func parsePcieFunction(ch chan<- prometheus.Metric, systemHostName string, pcieF
 	if pciFunctionHealthStateValue, ok := parseCommonStatusHealth(pciFunctionHealthState); ok {
 		ch <- prometheus.MustNewConstMetric(systemMetrics["system_pcie_function_health_state"].desc, prometheus.GaugeValue, pciFunctionHealthStateValue, systemPCIeFunctionLabelLabelValues...)
 	}
+}
+
+func duplicateDevices(devices []redfish.Device) []redfish.Device {
+	var (
+		count = make(map[string]int)
+		first = make(map[string]int)
+		index int
+	)
+
+	for i, v := range devices {
+		if _, ok := first[v.Name]; !ok {
+			first[v.Name] = i
+			count[v.Name] = 1
+			continue
+		}
+
+		index = count[v.Name] + 1
+		if index == 2 {
+			devices[first[v.Name]].Name = fmt.Sprintf("%s (%d)", v.Name, 1)
+		}
+		count[v.Name] = index
+
+		devices[i].Name = fmt.Sprintf("%s (%d)", v.Name, index)
+	}
+
+	return devices
 }
